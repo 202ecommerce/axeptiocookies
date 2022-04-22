601fd24252933a2750c12798598295bec27bb1c7
Denys <dbezuhlyi@202-ecommerce.com>
2022-04-22 17:04:11 +0200

    Added workflows; refs #33410

---
+++ 202/docker/run_for_unittest.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+set -x
+
+/etc/init.d/mariadb start
+
+echo "Init and install of Axeptio module"
+
+php /var/www/html/bin/console prestashop:module install axeptiocookies -e prod
+
+cd /var/www/html/modules/axeptiocookies/
+
+echo "Launch of unit tests"
+
+vendor/bin/phpunit -c test/phpunit.xml
+
+chown www-data:www-data /var/www/html/var -Rf
