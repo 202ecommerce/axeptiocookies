52e72e690ecf37e981a6bbf728346a17523e25e5
Denys <dbezuhlyi@202-ecommerce.com>
2022-06-27 18:40:27 +0200

    Composer install; refs #35513

--- 202/package.sh
+++ 202/package.sh
@@ -6,8 +6,8 @@ rm -Rf package-lock.json
 if [ -f "composer.json" ]; then
     echo "Exécutution de composer pour ${TARGETNAME}"
     echo "Exécutution de composer pour ${BUILD}" >> /tmp/v${TARGETVERSION}-${ENVIRONMENT}-${TARGETNAME}.txt
-    rm -Rf vendor/composer/installed.json
     rm -Rf composer.lock
-    composer remove phpstan/phpstan phpunit/phpunit prestashop/php-dev-tools prestashop/header-stamp --no-progress --dev --no-install --no-update --no-audit
-    composer update --no-dev --optimize-autoloader --classmap-authoritative
+    find ./vendor -mindepth 1 ! -regex '^./vendor/totpsclasslib\(/.*\)?' -delete
+    composer remove phpstan/phpstan phpunit/phpunit prestashop/php-dev-tools prestashop/header-stamp --no-progress --dev --no-install --no-update
+    composer install --no-dev --optimize-autoloader --classmap-authoritative
 fi
