8cceeac360f9b40938198454ca7fb4c94bb36d2b
Denys <dbezuhlyi@202-ecommerce.com>
2022-06-27 18:30:35 +0200

    Composer install; refs #35513

--- 202/package.sh
+++ 202/package.sh
@@ -2,3 +2,12 @@ rm -Rf views/_dev
 rm -Rf .php_cs.dist
 rm -Rf package.json
 rm -Rf package-lock.json
+
+if [ -f "composer.json" ]; then
+    echo "Exécutution de composer pour ${TARGETNAME}"
+    echo "Exécutution de composer pour ${BUILD}" >> /tmp/v${TARGETVERSION}-${ENVIRONMENT}-${TARGETNAME}.txt
+    rm -Rf vendor/composer/installed.json
+    rm -Rf composer.lock
+    composer remove phpstan/phpstan phpunit/phpunit prestashop/php-dev-tools prestashop/header-stamp --no-progress --dev --no-install --no-update --no-audit
+    composer update --no-dev --optimize-autoloader --classmap-authoritative
+fi
